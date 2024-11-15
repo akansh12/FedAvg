e2c53052e1165315ac9ef18e5ac205c14a6f7b82
akansh12 <akanshmaurya@gmail.com>
2024-11-15 17:38:44 +0100

    model_py

---
+++ src/model.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.fc1 = nn.Linear(1600, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.max_pool2d(x, 2)
+        x = torch.relu(self.conv2(x))
+        x = torch.max_pool2d(x, 2)
+        x = x.view(-1, 1600)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+    
+
+def get_model(config):
+    if config["model"] == "SimpleCNN":
+        return SimpleCNN()
+    else:
+        raise ValueError("Model not supported")
